f7808e90b3ff56014308e37ffd2eae15ebd02135
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2022-01-17 12:17:17 +0900

    Update Zurikela-s-Graph.cpp

--- Zurikela-s-Graph.cpp
+++ Zurikela-s-Graph.cpp
@@ -2,14 +2,64 @@
 #include <cstdio>
 #include <vector>
 #include <iostream>
+#include <fstream>
 #include <algorithm>
 using namespace std;
 
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
 
 int main()
 {
-    std::vector<int> result;
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string t_temp;
+    getline(cin, t_temp);
+
+    int t = stoi(ltrim(rtrim(t_temp)));
+    
     
     return 0;
 }
 
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
