71769461768ffc88df75820cc55266b7d7314cd9
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-10-05 10:41:54 +0900

    Update Zurikela-s-Graph.cs

--- Zurikela-s-Graph.cs
+++ Zurikela-s-Graph.cs
@@ -2,8 +2,33 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 
-class Solution {
-    static void Main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+class TreeDP {
+    public int n;
+    public int[] nodeValue;
+    public List<int>[] adj;
+    public int[][] dp;
+    public bool[] visited;
+
+    public TreeDP(int size) {
+        n = size;
+        nodeValue = new int[size + 1];
+        adj = new List<int>[size + 1];
+        for (int i = 0; i <= size; i++) adj[i] = new List<int>();
+        dp = new int[size + 1][];
+        for (int i = 0; i <= size; i++) dp[i] = new int[2];
+        visited = new bool[size + 1];
+    }
+
+    public void Dfs(int u, int parent) {
+        visited[u] = true;
+        dp[u][0] = 0;
+        dp[u][1] = nodeValue[u];
+
+        foreach (var v in adj[u]) {
+            if (v == parent) continue;
+            Dfs(v, u);
+            dp[u][0] += Math.Max(dp[v][0], dp[v][1]);
+            dp[u][1] += dp[v][0];
+        }
     }
 }
