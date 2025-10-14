fe498dc1b4fdc21580c4e47f2b935b0fa3b59d51
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-10-14 15:41:03 +0900

    Update Zurikela-s-Graph.java

--- Zurikela-s-Graph.java
+++ Zurikela-s-Graph.java
@@ -10,6 +10,17 @@ public class Solution {
         List<List<Integer>> adj;
         int[][] dp;
         boolean[] visited;
+
+        TreeDP(int size) {
+        n = size;
+        nodeValue = new int[size + 1];
+        adj = new ArrayList<>(size + 1);
+        for (int i = 0; i <= size; i++) {
+            adj.add(new ArrayList<>());
+        }
+        dp = new int[size + 1][2];
+        visited = new boolean[size + 1];
+        }        
     }
 
     public static void main(String[] args) throws IOException {
